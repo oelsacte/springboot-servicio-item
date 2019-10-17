09f5c6944516b650be58f88ea8cd620970441615
Octavio Elsacte Palma <oelsacte@gmail.com>
2019-10-17 15:46:13 -0500

    First commit

---
+++ src/test/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootServicioItemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
